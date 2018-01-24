7b0dd424d7f1047b2145640cf715ca91f480672f
mariano.aquino <mariano.aquino@gmail.com>
2018-01-24 16:40:32 -0300

    remove commented code (clean up)

--- src/controllers/GetAllTweetsController.js
+++ src/controllers/GetAllTweetsController.js
@@ -2,29 +2,12 @@
 GetAllTweets = require('../useCases/GetAllTweets');
 
 // external dependencies
-// const Twitter = require('twitter');
 
 class GetAllTweetsController {
 
     constructor (factory) {
         this.factory = factory;
-        // this.client = new Twitter({
-        //     consumer_key: "FarRAIgvRpc8kNv5n9gLcvTO4",
-        //     consumer_secret: "iwGrujzzqPGV1GbSpagVfUMNQ0BklY1tA3eQZDGPrBbHjVqbDV",
-        //     access_token_key: "2752102411-ht5jkkirIo5piUV7ux7d5kKuKcekg3NwjcP9mMD",
-        //     access_token_secret:"18N6DBBDJWEDLnoxwMGKhxTWF7VgQGrJuU5rvd1Da5PZ3"
-        // });
     }
-
-    // onFetchTweets() {
-    //     this.client.get('search/tweets', {q: "javascript"}, function(err, favs, response){
-    //         if (err){
-    //             onError(err);
-    //         } else {
-    //             onSuccess(favs);
-    //         }
-    //     });
-    // }
 
     onGetAllTweets(req, res){
         let repo = this.factory.createTweetsRepository();
